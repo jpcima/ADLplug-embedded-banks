5dcb3be47d88e25a7d6abd7963cbcefd4a435a66
JP Cimalando <jpcima@users.noreply.github.com>
2019-01-26 03:52:56 +0100

    update file organization and script

--- pak-banks.sh
+++ pak-banks.sh
@@ -1,11 +1,18 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
+shopt -s nullglob
 
 if test ! "$#" -eq 1; then
     echo "Usage: pak-banks <directory>"
     exit 1
 fi
 
-banks="$1"/*
+declare -a banks=()
+declare -a names=()
+for bank in "$1"/*.{wopl,wopn}; do
+    banks+=("$bank")
+    names+=("$(basename "${bank: : -5}" | sed -r 's/^[0-9]{3} //')")
+done
 
 cleanup() {
     rm -f _all.wopl.gz _all.wopl
@@ -18,18 +25,16 @@ if test -t 1; then
 fi
 
 # Compress
-cat $banks > _all.wopl
+cat "${banks[@]}" > _all.wopl
 zopfli _all.wopl
 
 # Write dictionary
 offset=0
-for bank in $banks; do
-    filename=`basename "$bank"`
-    filename=`echo "$filename" | sed -r 's/^[0-9]{3} //'`
-    size=`stat -c '%s' "$bank"`
+for i in "${!banks[@]}"; do
+    size=`stat -c '%s' "${banks[$i]}"`
     printf '%.8x' "$size" | xxd -r -p
     printf '%.8x' "$offset" | xxd -r -p
-    printf '%s' "$filename"
+    printf '%s' "${names[$i]}"
     printf '%.2x' 0 | xxd -r -p
     offset=$((offset+size))
 done
